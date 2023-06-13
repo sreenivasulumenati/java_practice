361927005f983ce1cb28ecfc8e97bd254854fc10
venu <venudodla999@gmail.com>
2023-06-14 00:06:42 +0530

    multiplication is added

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Multiplication {
+    public static void main(String args[]){
+        int a;
+        int b;
+        Scanner o=new Scanner(System.in);
+        System.out.println("Enter two numbers : ");
+        a=o.nextInt();
+        b=o.nextInt();
+        System.out.println("The multiplication of "+a+" and "+b+" is : "+(a*b));
+    }
+}

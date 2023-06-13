878d21c5eab1092d3678724b28c0b87c2a780ca3
venu <venudodla999@gmail.com>
2023-06-13 23:42:09 +0530

    addition and subtraction is done

---
+++ subtraction.java
@@ -0,0 +1,17 @@
+class subtraction
+{
+public static void main(String args[])
+{
+ int a=5;
+ int b=20;
+ int c=a+b;
+ int k= ++a + ++a + a++ + --a + a-- + ++a + a--;
+ System.out.println("The Addition Result is:" +c);
+ //System.out.println(--a);a=10-1=9
+ //System.out.println(a);
+ //System.out.println(a--);9 a=9-1=8
+ //System.out.println(k);
+ System.out.println(k);
+
+}
+}

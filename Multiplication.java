d9a6afcd7d51806ffa6c187908ec869785433cfa
swarnasureshbabu <swarnasureshbabu10@gmail.com>
2023-06-14 00:15:10 +0530

    added c variable in the multiplication

--- Multiplication.java
+++ Multiplication.java
@@ -4,10 +4,12 @@ class Multiplication {
     public static void main(String args[]){
         int a;
         int b;
+        int c;
         Scanner o=new Scanner(System.in);
         System.out.println("Enter two numbers : ");
         a=o.nextInt();
         b=o.nextInt();
-        System.out.println("The multiplication of "+a+" and "+b+" is : "+(a*b));
+        c=o.nextInt();
+        System.out.println("The multiplication of "+a+" and "+b+" and "+c+" is : "+(a*b*c));
     }
 }
